373f7d01db798e4aa88d416a558d9e312a8833fe
Mike Klimek <mike.klimek.developer@gmail.com>
2023-08-08 17:16:54 +0200

    Use new Kotlin 1.9 HexFormat

--- bluetooth/src/main/kotlin/cloud/mike/divelog/bluetooth/utils/ByteArrayExt.kt
+++ bluetooth/src/main/kotlin/cloud/mike/divelog/bluetooth/utils/ByteArrayExt.kt
@@ -1,6 +1,10 @@
+@file:OptIn(ExperimentalStdlibApi::class)
+
 package cloud.mike.divelog.bluetooth.utils
 
-import java.util.Locale
+private val hexFormat = HexFormat {
+    upperCase = true
+    bytes { byteSeparator = " " }
+}
 
-fun ByteArray.toHexString() = joinToString { it.toHexString() }
-fun Byte.toHexString() = String.format(Locale.ENGLISH, "%02X", this)
+fun ByteArray.toHexString() = toHexString(hexFormat)
