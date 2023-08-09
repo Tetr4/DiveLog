10d3c76fe200aa5a76e1e7a49d9614154442411d
Mike Klimek <mike.klimek.developer@gmail.com>
2023-08-10 00:58:44 +0200

    Revert "Add support for SARIF reports"
    This reverts commit ca0b1cf67247125e65e68340bd2a715d5f37a6d4.
    
    Code scanning is not available for private repositories, so
    undo this. We can enable it again, after repo goes public.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import io.gitlab.arturbosch.detekt.Detekt
-import io.gitlab.arturbosch.detekt.report.ReportMergeTask
-
 plugins {
     // AGP (https://developer.android.com/build/releases/gradle-plugin)
     id("com.android.application") version "8.1.0" apply false
@@ -9,14 +6,8 @@ plugins {
     // Kotlin (https://kotlinlang.org/docs/releases.html#release-details)
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 
-    // detekt (https://github.com/detekt/detekt#requirements)
+    // Detekt (https://github.com/detekt/detekt#requirements)
     id("io.gitlab.arturbosch.detekt") version "1.23.1"
-}
-
-// This task will merge every module's generated detekt report
-// See https://detekt.dev/docs/introduction/reporting/#merging-reports
-val reportMerge by tasks.registering(ReportMergeTask::class) {
-    output.set(rootProject.layout.buildDirectory.file("reports/detekt/merge.sarif"))
 }
 
 // Use detekt in all modules
@@ -28,11 +19,5 @@ subprojects {
     }
     dependencies {
         detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.1") // ktlint rules
-    }
-    tasks.withType<Detekt>().configureEach {
-        finalizedBy(reportMerge)
-    }
-    reportMerge {
-        input.from(tasks.withType<Detekt>().map { it.sarifReportFile })
     }
 }
