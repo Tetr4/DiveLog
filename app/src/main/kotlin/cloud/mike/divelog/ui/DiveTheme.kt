3474c2feb88789ee48697b9397c95398d448de14
Mike Klimek <mike.klimek.developer@gmail.com>
2023-08-12 19:28:37 +0200

    Implement Material You support

--- app/src/main/kotlin/cloud/mike/divelog/ui/DiveTheme.kt
+++ app/src/main/kotlin/cloud/mike/divelog/ui/DiveTheme.kt
@@ -1,9 +1,14 @@
 package cloud.mike.divelog.ui
 
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.themeadapter.material3.Mdc3Theme
 
@@ -11,7 +16,17 @@ import com.google.accompanist.themeadapter.material3.Mdc3Theme
 fun DiveTheme(
     content: @Composable () -> Unit,
 ) {
-    Mdc3Theme(content = content)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        // Use user wallpaper colors (Material You)
+        val context = LocalContext.current
+        val colors = if (isSystemInDarkTheme()) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        MaterialTheme(
+            colorScheme = colors,
+            content = content,
+        )
+    } else {
+        Mdc3Theme(content = content)
+    }
 }
 
 @Immutable
