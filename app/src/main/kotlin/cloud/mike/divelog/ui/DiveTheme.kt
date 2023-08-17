4a50aeeca86a97062ae445f4adf3eff45eba88fa
Mike Klimek <mike.klimek.developer@gmail.com>
2023-08-17 23:13:52 +0200

    Fix typography and shapes not used together with dynamic colors

--- app/src/main/kotlin/cloud/mike/divelog/ui/DiveTheme.kt
+++ app/src/main/kotlin/cloud/mike/divelog/ui/DiveTheme.kt
@@ -3,6 +3,7 @@ package cloud.mike.divelog.ui
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -13,20 +14,24 @@ import androidx.compose.ui.unit.dp
 import com.google.accompanist.themeadapter.material3.Mdc3Theme
 
 @Composable
-fun DiveTheme(
-    content: @Composable () -> Unit,
-) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-        // Use user wallpaper colors (Material You)
-        val context = LocalContext.current
-        val colors = if (isSystemInDarkTheme()) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        MaterialTheme(
-            colorScheme = colors,
-            content = content,
-        )
-    } else {
+fun DiveTheme(content: @Composable () -> Unit) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
         Mdc3Theme(content = content)
+    } else {
+        // Use colors based on user's wallpaper ("Material You")
+        MaterialTheme(colorScheme = dynamicColorScheme()) {
+            Mdc3Theme(
+                readColorScheme = false,
+                content = content,
+            )
+        }
     }
+}
+
+@Composable
+private fun dynamicColorScheme(): ColorScheme {
+    val context = LocalContext.current
+    return if (isSystemInDarkTheme()) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
 }
 
 @Immutable
