9e3385030731db93c7cfc3a7d77c8ccf12c85d4a
Mike Klimek <mike.klimek.developer@gmail.com>
2024-06-06 23:41:16 +0200

    Localize temperatures and distances

--- app/src/main/kotlin/cloud/mike/divelog/localization/Formatting.kt
+++ app/src/main/kotlin/cloud/mike/divelog/localization/Formatting.kt
@@ -1,8 +1,16 @@
 package cloud.mike.divelog.localization
 
+import android.icu.text.MeasureFormat
+import android.icu.text.MeasureFormat.FormatWidth
+import android.icu.util.LocaleData
+import android.icu.util.Measure
+import android.icu.util.MeasureUnit
+import android.icu.util.ULocale
+import android.os.Build
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.text.util.LocalePreferences
 import java.text.NumberFormat
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -23,21 +31,35 @@ fun Number.formatDiveNumber(): String = "#${this.format()}"
 @Composable
 @ReadOnlyComposable
 fun Number.formatDepthMeters(): String {
-    val numberFormat = NumberFormat.getInstance(primaryLocale).apply {
-        maximumFractionDigits = 2
+    val imperial = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        LocaleData.getMeasurementSystem(ULocale.forLocale(primaryLocale)) == LocaleData.MeasurementSystem.US
+    } else {
+        primaryLocale.country == "US"
     }
-    return "${numberFormat.format(this)} m"
+    val converted = if (imperial) this.toDouble() * 3.28084 else this.toDouble()
+    val measureUnit = if (imperial) MeasureUnit.FOOT else MeasureUnit.METER
+    return MeasureFormat
+        .getInstance(primaryLocale, FormatWidth.SHORT)
+        .format(Measure(converted, measureUnit))
 }
 
 @Composable
 @ReadOnlyComposable
 fun Number.formatTemperatureCelsius(): String {
-    // TODO Use new API once SDK 34 is stable:
-    //   https://developer.android.com/reference/androidx/core/text/util/LocalePreferences#getTemperatureUnit()
-    val numberFormat = NumberFormat.getInstance(primaryLocale).apply {
-        maximumFractionDigits = 2
+    val temperatureUnit = LocalePreferences.getTemperatureUnit(primaryLocale)
+    val converted = when (temperatureUnit) {
+        LocalePreferences.TemperatureUnit.FAHRENHEIT -> (this.toDouble() * 9 / 5) + 32
+        LocalePreferences.TemperatureUnit.KELVIN -> this.toDouble() + 273.15
+        else -> this.toDouble() // Celsius
     }
-    return "${numberFormat.format(this)} °C"
+    val measureUnit = when (temperatureUnit) {
+        LocalePreferences.TemperatureUnit.FAHRENHEIT -> MeasureUnit.FAHRENHEIT
+        LocalePreferences.TemperatureUnit.KELVIN -> MeasureUnit.KELVIN
+        else -> MeasureUnit.CELSIUS
+    }
+    return MeasureFormat
+        .getInstance(primaryLocale, FormatWidth.SHORT)
+        .format(Measure(converted, measureUnit))
 }
 
 @Composable
